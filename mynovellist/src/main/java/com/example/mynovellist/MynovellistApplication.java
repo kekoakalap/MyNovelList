94afaac4bcab51272a2f2f19f37905785c370d13
Mackenzie Borneo Dela Cruz <mackenzie.delacruz@cit.edu>
2025-02-06 23:44:17 +0800

    Setup work environment

---
+++ mynovellist/src/main/java/com/example/mynovellist/MynovellistApplication.java
@@ -0,0 +1,13 @@
+package com.example.mynovellist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynovellistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynovellistApplication.class, args);
+	}
+
+}
